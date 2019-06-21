9f91bfb8d7271438c3c7e6ca591e4f710fb8da5a
cyc00201 <you@example.com>
2019-06-21 18:52:11 +0800

    test

---
+++ test65/ main.swift
@@ -0,0 +1,20 @@
+//
+//   main.swift
+//  test65
+//
+//  Created by User06 on 2019/6/18.
+//  Copyright © 2019 User05. All rights reserved.
+//
+
+import UIKit
+
+class main: UITableViewController {
+    
+    var test: Any! = nil
+    override func viewDidLoad(){
+        print(test)
+    }
+    
+}
+
+
---
+++ test65/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  test65
+//
+//  Created by User05 on 2019/6/5.
+//  Copyright © 2019 User05. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+   
+    
+
+        override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
